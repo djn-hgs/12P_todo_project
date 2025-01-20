d63d17b7969e3ec30bf787b140d12a217504e543
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2025-01-20 10:06:48 +0000

    Refactored as MVC

--- create_todo_db.py
+++ create_todo_db.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('todo.sqlite')
 cursor = conn.cursor()
 
 create_todo_table = '''
-CREATE TABLE IF NOT EXISTS todo (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS todo 
+(id INTEGER PRIMARY KEY AUTOINCREMENT,
 task TEXT NOT NULL,
 status BOOLEAN NOT NULL
 );'''
